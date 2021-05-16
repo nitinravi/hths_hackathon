671c29777a52c5149b9966660cfd43251e6a1026
nitinravi <nitinravi18@gmail.com>
2021-05-16 15:01:32 +0530

    input device setup

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ import queue
 
 import music21
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-input", required=False, type=int, help="Audio Input Device")
+args = parser.parse_args()
+
+if not args.input:
+    print("No input device specified. Printing list of input devices now: ")
+    p = pyaudio.PyAudio()
+    for i in range(p.get_device_count()):
+        print("Device number (%i): %s" % (i, p.get_device_info_by_index(i).get('name')))
+    print("Run this program with -input 1, or the number of the input you'd like to use.")
+    exit()
+
